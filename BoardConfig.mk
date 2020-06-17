2d338a61da03e07cbf9bc035c7bcf73cf59b51b4
zqm64 <zqm64@outlook.com>
2020-06-17 11:57:17 +0800

    j5ltechn: Switch to use NFC stuff from msm8916-common
    Change-Id: I724dcc3ab765027bc6336e5438b94d949b184813

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_OTA_ASSERT_DEVICE := j5ltechn,j5ltezm
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j5lte_chncmcc_defconfig
 
+# NFC
+-include device/samsung/msm8916-common/nfc/pn547/board.mk
+
 # Radio
 SIM_COUNT := 2
 
